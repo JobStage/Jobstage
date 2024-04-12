a3d6433f6b4a7d2e4e239ca51e0d59cf4a7c7113
NelsonSilvaa <nelsonolech@gmail.com>
2024-04-12 17:58:39 -0300

    incluindo bootstrap v5.3 localmente e atualizando arquivo template

--- app/public/html/template.php
+++ app/public/html/template.php
@@ -5,22 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TEMPLATE</title>
-    <link rel="stylesheet" href="./app/public/css/sidebar.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="../app/public/css/sidebar.css">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
-    <script src="./app/public/js/jquery-3.7.1.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../app/lib/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="../app/public/css/sweetalert2.min.css">
+    <script src="../app/public/js/jquery-3.7.1.js"></script>
 </head>
 <body>
 <div class="main-container d-flex">
-    <?php require_once  "./app/public/html/sidebar.html"?>
+    <?php require_once  "../app/public/html/sidebar.html"?>
     <div class="content">
-        <?php require_once "./app/public/html/navbar.html" ?>
+        <?php require_once "../app/public/html/navbar.html" ?>
         <div class="container">
-            <!-- COLOCAR O CONTEUDO DENTRO DO CONTAINER -->
+
+            <!-- CONTEUDO AQUI -->
+            
         </div>
     </div>
 </div>
-<script src="./app/public/js/sidebar.js"></script>
+<script src="../app/public/js/sidebar.js"></script>
+<script src="../app/lib/bootstrap/bootstrap.bundle.min.js"></script>
+<script src="../app/public/js/sweetalert2.all.min.js"></script>
 </body>
 </html>
