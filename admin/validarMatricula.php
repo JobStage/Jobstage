1073b91da5172a639edbc4b60a9394dce12f507c
NelsonSilvaa <nelsonolech@gmail.com>
2024-08-26 11:48:15 -0300

    criando login e tela de validacao de matricula

---
+++ admin/validarMatricula.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+require_once '../app/controller/matricula.php';
+$matricula = new matricula();
+
+ob_start();  
+?>
+
+
+<div class="card">
+    <div class="card-body">
+        <div style="display: flex; flex-direction:column;">
+            <?= $matricula->listarMatriculas() ?>
+        </div>
+    </div>
+</div>
+<script src="../app/public/js/dadosAluno.js"></script>
+
+<?php
+$content = ob_get_clean(); 
+$pageTitle = "Validar Matriculas"; 
+include('../app/public/html/template.php'); 
+?>
