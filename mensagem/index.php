12ac3f2a694fd980e1f9e80d26b95188f40cfb59
Julio Cesar <julio.aquino07092002@gmail.com>
2024-09-05 11:18:58 -0300

    reagindo a mensagens

--- mensagem/index.php
+++ mensagem/index.php
@@ -2,4 +2,13 @@
 use Swoole\jobstage\mensagem;
 $servidor = new Server('0.0.0.0', 8080); 
 
+$servidor->on('message', function (Server $servidor, Frame, $mensagem){
+  $conexoes = $servidor->connections;
+  $origem = $mensagem->fd;
+
+  foreach($conexoes as $conexao){
+    if($conexao === $origem) continue;
+    $servidor->push($conexao, json_encode(['type' => 'chat', 'text' => $mensagem->data]));
+  }
+});
 $servidor->start();
