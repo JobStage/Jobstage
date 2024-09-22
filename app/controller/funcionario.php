140f55fc8d74c9f0c76fc927c383a0e856dd3002
NelsonSilvaa <nelsonolech@gmail.com>
2024-09-22 11:41:34 -0300

    criando tela de funcionarios, funcao de salvar e listar

---
+++ app/controller/funcionario.php
@@ -0,0 +1,55 @@
+<?php
+// if(!isset($_SESSION)) 
+// { 
+//     session_start(); 
+// } 
+require_once __DIR__ . '/../model/FuncionarioModel.php';
+require_once 'matricula.php';
+
+class funcionarioController{
+    private $funcionario;
+
+ 
+
+    public function __construct() {
+       $this->funcionario = new funcionarioModel();
+     
+    }
+
+    public function salvar($nome, $email, $idEmpresa){
+        if(empty($nome) || empty($email)){
+            return;
+        }
+
+        if($this->funcionario->salvar($nome, $email, $idEmpresa)){
+            $retorno = array('success' => true, 'tittle' => 'Sucesso!', 'msg' => 'Funcionário criado!', 'icon' => 'success');
+            echo json_encode($retorno);
+            return $retorno;
+        }
+        $retorno = array('success' => false, 'tittle' => 'Erro!', 'msg' => 'Erro ao criar um funcionário!', 'icon' => 'error');
+        echo json_encode($retorno);
+        return $retorno;
+    }
+
+    public function listarFuncionario($idEmpresa){
+        $html = '';
+        $funcionario = $this->funcionario->listarFuncionarios($idEmpresa);
+        if($funcionario){
+            foreach($funcionario as $value){
+                $html .= '
+                    <div>
+                        Nome: '.$value['nome'].'
+                        E-mail: '.$value['email'].'
+                    </div>';
+            }
+            return $html;
+        }
+        $html = '<div class="alert alert-danger" role="alert">
+                    Não existe nenhum funcionário cadastrado
+                </div>';
+
+        return $html;
+    }
+
+}
+
---
+++ app/requests/funcionario.php
@@ -0,0 +1,21 @@
+<?php
+if(!isset($_SESSION)) 
+{ 
+    session_start(); 
+} 
+require_once '../controller/funcionario.php';
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $acao = $_POST['acao']?? null;
+    $nome = $_POST['nome'] ?? '';
+    $email = $_POST['email'] ?? '';
+    $formacao = new funcionarioController();
+    switch($acao){
+        case 'salvar':
+           $formacao->salvar($nome, $email, $_SESSION['id']);
+        break;
+        case 'editar':
+        break;
+    }
+}
